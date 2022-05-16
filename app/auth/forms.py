5c6376445ce4427962c64a335c8e545836d6fb75
Robert <rgitonga.07@gmail.com>
2022-05-16 10:59:06 +0300

    initial file set up

---
+++ app/auth/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
+from wtforms.validators import Required,Email,EqualTo
+
+from ..models import User
+
+
+
---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for,abort,flash
+from . import main
+
+
+@main.route('/')
+def index():
+
+
+    return render_template('main/index.html')
